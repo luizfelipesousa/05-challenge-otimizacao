31351eb35712f7377d1df37c37df1cc1dd59ed1a
Luiz Felipe <luizfelipesousa@hotmail.com>
2022-02-10 21:30:47 -0300

    ReactJs - Desafio de otimização de aplicativos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,30 @@
-import { Icon } from './Icon';
+import { Icon } from "./Icon";
 
-import '../styles/button.scss';
-import { ButtonHTMLAttributes } from 'react';
+import "../styles/button.scss";
+import { ButtonHTMLAttributes, memo } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
-  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  iconName: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
   selected: boolean;
 }
 
-export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
+export function ButtonFunction({
+  iconName,
+  title,
+  selected,
+  ...rest
+}: ButtonProps) {
+  const color = selected ? "#FAE800" : "#FBFBFB";
+  const styledClass = selected && { className: "selected" };
   return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+    <button type="button" {...styledClass} {...rest}>
+      <Icon name={iconName} color={color} />
       {title}
     </button>
   );
 }
+
+export const Button = memo(ButtonFunction, (prevProps, nextProps) => {
+  return Object.is(prevProps.selected, nextProps.selected);
+});
